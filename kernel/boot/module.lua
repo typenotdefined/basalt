93c8c95a096539bd9cd36e37b2d5c3d7ad4d0c15
UniQodex <192822098+typenotdefined@users.noreply.github.com>
2026-02-10 18:21:21 +0100

    add readme

--- kernel/boot/module.lua
+++ kernel/boot/module.lua
@@ -2,13 +2,20 @@
         Module loader
 
     Used to load kernel and schema modules. Uses underlaying includer system.
+
+    About moduleDeclare:
+    It serves as a way to check plugin collisions, altough it adds a more tedious work it brings
+    10x better reliability, as in helix framework, when someone installed it and downloaded like every
+    avaible public plugin and smashed them together, it resulted in every plugin using/overwriting the same hook
+    ultimately leading to crashando. In this way we compare every module moduleDeclare with each other and if collides
+    we will notify the system operator and disable them until they are resolved or one is disabled.
 ]]--
 
 Basalt.Module = Basalt.Module or {}
 Basalt.Module.Data = Basalt.Module.Data or {}
 
 function Basalt.Module.InitializeModules(callback)
-    local problemsFound
+    local problemsFound = 0
 
     local _, moduleFolders = file.Find("basalt/modules/*", "LUA", "nameasc")
 
@@ -21,6 +28,7 @@ function Basalt.Module.InitializeModules(callback)
         if(file.Exists("basalt/modules/"..moduleFolders[i].."/mod.lua")) then
             local moduleData = Basalt.Includer.IncludeFile("basalt/modules/"..moduleFolders[i].."/mod.lua", Basalt.RealmType.SHARED)
 
+            -- check if everything is existing as it should be
             if(!moduleData.ModuleName) then
                 Basalt.Console.PrintWarning("(MOD_CONFIG_ERR) Not found ModuleName in ("..moduleFolders[i]..")! Using raw path name instead...")
                 moduleData.ModuleName = moduleFolders[i]
@@ -33,9 +41,10 @@ function Basalt.Module.InitializeModules(callback)
                 Basalt.Console.PrintWarning("(MOD_CONFIG_ERR) Not found ModuleDeclare for ("..moduleData.ModuleName..")!\n[ !!! ] This module will be disabled for reliability reasons!\n")
                 Basalt.Module.Data[moduleData.ModuleName].STATUS = "[ FAIL ] Missing ModuleDeclare"
                 problemsFound = problemsFound + 1
-                return
+                continue
             end
 
+            -- end check
             if(problemsFound == 0) then
                 Basalt.Console.PrintOK("Module ("..moduleData.ModuleName..") loaded with no errors")    
             else
